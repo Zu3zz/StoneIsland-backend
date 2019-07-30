d57dafecb5152b12f89d96c31441d52aed19849e
Zu3zz <zthzththth@163.com>
2019-07-30 23:48:45 +0800

    finish exception intercept

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const Koa = require('koa')
 const parser=  require('koa-bodyparser')
 const InitManager = require('./core/init')
+const catchError = require('./middlewares/exceptions')
 
 const app = new Koa()
 
 app.use(parser())
+app.use(catchError)
 
 // 注册路由
 InitManager.initCore(app)
--- app/api/v1/book.js
+++ app/api/v1/book.js
@@ -1,6 +1,8 @@
 const Router = require('koa-router')
 const router = new Router()
 
+const {HttpException, ParameterException} = require('../../../core/http-exception')
+
 router.post('/v1/:id/book/latest', (ctx, next) => {
   // 获取path headers query body四个参数
   // postman 发送post请求 http://localhost:3000/v1/23/book/latest?qq=773835 自定义一个header name: 3zz 自定义body内容
@@ -8,6 +10,12 @@ router.post('/v1/:id/book/latest', (ctx, next) => {
   const query = ctx.request.query // post请求中?后面带的参数 qq: "773835"
   const headers = ctx.request.header // post请求中http的header部分 也可以带参数 name: "3zz"
   const body = ctx.request.body // post请求中的body部分 可以选择文件格式 通常是json 需要通过koa-bodyparser获取
+  if(true){
+    // const error = new global.errs.ParameterException()
+    const error = new ParameterException()
+    // error.requestUrl = `${ctx.method} ${ctx.path}`
+    throw error
+  }
 
   ctx.body = {
     key: 'book'
